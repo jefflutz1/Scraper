43c179bb84f04f1fd091764f6970e81901ed3ad8
jefflutz1 <jlutz155@gmail.com>
2019-07-01 11:58:10 -0700

    scraping

---
+++ plot.py
@@ -0,0 +1,66 @@
+import matplotlib.pyplot as plt
+import pickle
+
+def readFile():
+    with open('changeData.data', 'rb') as filehandle:  
+        outputList = pickle.load(filehandle)
+    
+    return outputList
+
+testList = readFile()
+
+def side_values(num_list):
+    results_list = sorted(num_list)
+    return results_list[0], results_list[-1]
+
+def min_max(inputList):
+    minimum = 999999
+    maximum = -999999
+    
+    for x in inputList:
+        if(float(x[0]) > maximum):
+            maximum = float(x[0])
+            
+        if(float(x[1]) > maximum):
+            maximum = float(x[1])
+            
+        if(float(x[2][0]) > maximum):
+            maximum = float(x[2][0])
+            
+        if(float(x[0]) < minimum):
+            minimum = float(x[0])
+        
+        if(float(x[1]) < minimum):
+            minimum = float(x[1])
+            
+        if(float(x[2][0]) < minimum):
+            minimum = float(x[2][0])
+    
+    return minimum,maximum
+
+x1_val = [float(x[0]) for x in testList]
+x2_val = [float(x[1]) for x in testList]
+x3_val = [float(x[2]) for x in testList]
+x4_val = [float(x[3][0]) for x in testList]
+y_val = []
+
+for i in range(len(testList)):
+    y_val.append(i)
+
+plt.plot(y_val,x1_val, label = "BTC")
+
+plt.plot(y_val,x2_val, label = "Top 10 Coins")
+
+plt.plot(y_val,x3_val, label = "Top 100 Coins")
+
+name = str(testList[0][3][1]) + " Coins"
+plt.plot(y_val,x4_val, label = name)
+
+plt.suptitle('24 hr Percent Change Over Time', fontsize=20)
+plt.xlabel('Time (15 * x seconds)', fontsize=18)
+plt.ylabel('Percent Change', fontsize=16)
+
+plt.legend()
+plt.savefig('differences.png')
+
+
---
+++ scrape.py
@@ -0,0 +1,80 @@
+import requests
+import re
+import pickle
+import time
+
+requests.packages.urllib3.disable_warnings() 
+
+pattern = re.compile('<tr id.*?</tr>',re.DOTALL)
+patter2 = re.compile('class="price" data-usd=\".*?\"')
+pattern3 = re.compile('\d+.\d+')
+pattern4 = re.compile('[-|\d]+\.\d+%')
+
+def getTop100():
+    response = requests.get('https://coinmarketcap.com/', verify=False)
+    result = pattern4.findall(response.text)
+    average100 = 0
+    
+    for i in result:
+        average100 += float(i[:-1])
+
+    average100 /= len(result)
+    
+    return format(float(average100),'.2f')
+
+def getTop10():
+    response = requests.get('https://coinmarketcap.com/', verify=False)
+    result = pattern4.findall(response.text)
+    average10 = 0
+    
+    for i in range(10):
+        average10 += float(result[i][:-1])
+
+    average10 /= 10
+    
+    return format(float(average10),'.2f')
+
+def getAll():
+    response = requests.get('https://coinmarketcap.com/all/views/all/', verify=False)
+    result = pattern4.findall(response.text)
+    average = 0
+    for i in result:
+        average += float(i[:-1])
+
+    average /= len(result)
+    return format(float(average),'.2f'), len(result)
+
+def getBTC():
+    response = requests.get('https://coinmarketcap.com/', verify=False)
+    result = pattern4.findall(response.text)
+    
+    return format(float(result[0][:-1]),'.2f')
+
+def writeToFile(inputList):
+    with open('changeData.data', 'wb') as filehandle:  
+        pickle.dump(inputList, filehandle)
+
+def readFile():
+    with open('changeData.data', 'rb') as filehandle:  
+        outputList = pickle.load(filehandle)
+    
+    return outputList
+'''
+print("BitCoin Change: " + str(getBTC()) + '%')
+print("Top 100 coins average: " + str(getTop100()) + '%')
+print(str(getAll()[1]) + " coins average: " + str(getAll()[0]) + '%')
+
+'''
+try:
+    changeList = readFile()
+except:
+    changeList = []
+
+temp = 0
+while(True): 
+    changeList.append((getBTC(),getTop10(),getTop100(), getAll()))
+    temp += 1
+    print(str(temp))
+    writeToFile(changeList)
+    
+    time.sleep(15)
